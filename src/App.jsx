6088e8f31e71653ac5069750303355d9db7d1885
jeankox <jeanko13@gmail.com>
2025-04-20 18:37:17 -0500

    style depuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Navigationpanel from './components/Navigationpanel'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,27 +9,12 @@ function App() {
 
   return (
     <>
-    <h3 class="text-3xl text-black" >holiwi</h3>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="flex flex-row w-screen h-screen bg-amber-50" >     
+          <Navigationpanel class="w-1/4"/>
+          <div class="w-full bg-red-50">
+            <h3 class="text-black">zona secundaria</h3>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
