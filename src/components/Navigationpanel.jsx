c95f57694eea54b223637d555ded35a33a1b09dc
jeankox <jeanko13@gmail.com>
2025-04-20 19:00:32 -0500

    navigation panel

--- src/components/Navigationpanel.jsx
+++ src/components/Navigationpanel.jsx
@@ -1,14 +1,18 @@
-import profilelogo from '../assets/profile.png'
+import profilelogo from '../assets/navigation/profile.png'
+
 
 const Navigationpanel = () => {
   return (
     <div class="w-1/5 bg-black  " > {/* main estructure */}
     
-          <div class="text-5xl p-5 m-5" >   
+          <div class="text-5xl p-5 m-5 mt-20" >   
               <img src={profilelogo}></img>{/* profile and advance button */ }
+              {/* categories button goes here */}
+          </div>
+          <div class="m-5"> {/* navigation buttons*/}
+            
 
           </div>
-
     
     </div>
   )
